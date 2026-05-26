6f2f13fca492d494b34e6e6f843ed05de36f8dcb
heis-oo <colan.2007@yahoo.com>
2026-05-26 15:44:57 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -12,7 +12,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "dnd",
+    status: "online",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "discord.gg",
         // Activity type number (0 = Playing).
-        type: 3, 
+        type: 1, 
       },
     ],
   },
